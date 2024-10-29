a9c8c1ef51c0e13b04f2fe27e6437c933fb48181
iletai <iletai@hotmail.com>
2024-10-29 21:03:53 +0700

    + Update Package

--- Package.swift
+++ Package.swift
@@ -5,22 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "BaseNavigationStack",
-    platforms: [
-        .macOS(.v13), .iOS(.v17)
-    ],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BaseNavigationStack",
-            targets: ["BaseNavigationStack"]),
+            targets: ["BaseNavigationStack"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BaseNavigationStack"),
+            name: "BaseNavigationStack"
+        ),
         .testTarget(
             name: "BaseNavigationStackTests",
-            dependencies: ["BaseNavigationStack"]),
+            dependencies: ["BaseNavigationStack"]
+        ),
     ]
 )
